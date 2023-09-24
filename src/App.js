3a35f12048b294bad9965fc1ca9abbb49c1049cd
Aavash kc <kcaavash244@gmail.com>
2023-09-24 14:03:58 +0545

    TextUtils done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import { useState } from "react";
 import Textform from "./components/Textform";
 import Alert from "./components/Alert";
-import About from "./components/About";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import About from "./components/About";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
@@ -61,22 +61,27 @@ function App() {
     }
   }
   return (
-    <Router>
+  <div>
+    
+
+    {/* // <Router> */}
       {/* <Navbar title="textUtils" aboutTitle="About"/> When we don't use default props then it is used */}
       <Navbar mode={mode} toggleMode={toggleMode} toggleRedMode={toggleRedMode}/>
       <Alert alert={alert}/>
       <div className="container">
         {/* /users -->component 1
         /users/home -->component 2  exact used to match complete location not partial */}
-        <Routes>
-          <Route exact path="/about" element={<About/>}>
-            </Route>
-          <Route exact path="/" element={<Textform showAlert={showAlert} heading="Enter text to analyze below" mode={mode} />}>
-          </Route>
-        </Routes>
+        {/* <Routes> */}
+          {/* <Route exact path="/about" element={<About/>}>
+            </Route> */}
+          {/* <Route exact path="/" element=> */}
+          {<Textform showAlert={showAlert} heading="Enter text to analyze below" mode={mode} />}
+          {/* </Route> */}
+        {/* </Routes> */}
         {/* <About/> */}
       </div>
-      </Router>
+      {/* // </Router> */}
+      </div>
   );
 }
 
